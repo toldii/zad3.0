bc85e6ea817205dedc888264b029a4d5ebdf8be7
toldii1 <100698275+toldii@users.noreply.github.com>
2022-04-12 12:14:23 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+class Main {
+  public static void main(String[] args) {
+    try {
+      Service1 s = new Service1();
+      s.addStudent(new Student("Krzysztof", 20));
+      s.addStudent(new Student("Janusz", 40));
+
+      var students = s.getStudents();
+      for(Student current : students) {
+        System.out.println(current.ToString());
+      }
+    } catch (IOException e) {
+
+    }
+  }
+}
